df6dcc71df2bfb09cc20107548daf0e7635603f8
nidhijagga <nidhijagga5239@gmail.com>
2023-01-27 16:24:41 +0530

    task10

---
+++ task10.js
@@ -0,0 +1,7 @@
+//When would you use --save-dev and --save
+
+//--save is used when you want to add a package as a dependency in your project, meaning the package is required for the project to run properly. --save-dev is used when you want to add a package as a development dependency, meaning the package is only needed for development and testing, but not for the actual running of the project.
+
+//What problem does nodemon solve?
+
+//Helps to run the files automatically whenever there is some changes in the code.
